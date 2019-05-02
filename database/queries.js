4b892181d6d9bcd336d807eb29c7ec91bf19f114
Jeena Chemparathy <jeena@Jeenas-MacBook-Pro.local>
2019-05-02 11:38:00 -0700

    add console.log

--- database/queries.js
+++ database/queries.js
@@ -13,7 +13,8 @@ const getEarningsById = (request, response) => {
   const query = request.params.query;
     earnings.query(`SELECT * FROM quarter WHERE company_id = ${query}`, (error, results) => {
       if (error) {
-        throw error
+        console.log('This error is made inside of queries.js in service: ',error);
+        response.status(403);
       }
       response.status(200).json(results.rows)
     })
