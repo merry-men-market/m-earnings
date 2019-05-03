38b9ef2cda24c07d24042347ce80000bed3618c0
Jeena Chemparathy <jeena@Jeenas-MacBook-Pro.local>
2019-05-03 11:35:43 -0700

    Remove 1 from variable names

--- server/server.js
+++ server/server.js
@@ -1,34 +1,34 @@
-const newrelic = require('newrelic');
+require('newrelic');
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+// const cors = require('cors');
 // const morgan = require('morgan');
 const path = require('path');
 // const Earnings = require('../database/Earning/EarningScheme');
 const earningDb  = require('../database/queries.js');
-const app1 = express();
-const port1 = process.env.PORT || 3002;
+const app = express();
+const port = process.env.PORT || 3002;
 const db = require('../database/index.js');
 
-app1.use(cors());
-// app1.use(morgan('tiny'));
-app1.use(bodyParser.json());
-app1.use(bodyParser.urlencoded({ extended: true }));
-app1.use(express.static(`${__dirname}/../public/`));
-app1.use('/earnings/:query', express.static(path.join(__dirname, '../public')));
-app1.locals.newrelic = newrelic;
+// app.use(cors());
+// app.use(morgan('tiny'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(`${__dirname}/../public/`));
+app.use('/earnings/:query', express.static(path.join(__dirname, '../public')));
 
 
-app1.get('/api/earnings/:query', earningDb.getEarningsById);
+app.get('/api/earnings/:query', earningDb.getEarningsById);
 
-app1.use('/', express.static(path.join(__dirname, '../public')));
+app.use('/', express.static(path.join(__dirname, '../public')));
 
-// app1.get('/:id', (req, res) => {
+
+// app.get('/:id', (req, res) => {
 //   res.status(200).sendFile(path.resolve(__dirname, '../public/index.html'));
 // });
 
 // READ - SDC
-// app1.get('/api/earnings', (req, res) => {
+// app.get('/api/earnings', (req, res) => {
   // set Default data equal to 001
     // db.getEarning("001", (data) => {
     //   res.status(200).json(data)
@@ -43,7 +43,7 @@ app1.use('/', express.static(path.join(__dirname, '../public')));
 
 
 
-// app1.get('/api/earnings/:id', (req, res) => {
+// app.get('/api/earnings/:id', (req, res) => {
   // set Default data equal to 001
     // db.getEarning(req.params.id, (data) => {
     //   res.status(200).json(data)
@@ -56,7 +56,7 @@ app1.use('/', express.static(path.join(__dirname, '../public')));
 
 // POST - SDC
 
-// app1.post('/api/earnings', (req, res) => {
+// app.post('/api/earnings', (req, res) => {
 //   const { body } = req;
 //    console.log('body: ', body);
 //   if(!body.id || !body.company || !body.actualEarning || !body.estimatedEarning || !body.quarter || !body.quarterNumber) {
@@ -75,7 +75,7 @@ app1.use('/', express.static(path.join(__dirname, '../public')));
 
 // UPDATE - SDC
 
-// app1.patch('/api/earnings/:id', function(req, res) {
+// app.patch('/api/earnings/:id', function(req, res) {
 //   var id =  req.params.id;
 //   Earnings.findOneAndUpdate({id: id}).exec((err, docs) => {
 //     if (err){
@@ -87,7 +87,7 @@ app1.use('/', express.static(path.join(__dirname, '../public')));
 
 // DELETE - SDC
 
-// app1.delete('/api/earnings/:id', function (req, res) {
+// app.delete('/api/earnings/:id', function (req, res) {
 //   var id = req.params.id;
 //   Earnings.deleteOne({ id: id }, (error) => {
 //     if (error){
@@ -99,7 +99,7 @@ app1.use('/', express.static(path.join(__dirname, '../public')));
 //   });
 // });
 
-app1.listen(port1, () => {
-  console.log(`server running at: ec2-52-53-226-249.us-west-1.compute.amazonaws.com:${port1}`);
+app.listen(port, () => {
+  console.log(`server running at: ec2-52-53-226-249.us-west-1.compute.amazonaws.com:${port}`);
 });
 
